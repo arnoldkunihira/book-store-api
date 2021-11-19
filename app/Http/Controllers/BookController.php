84303472709660cfee0aff186b71ddc9919ae3b8
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-20 00:38:18 +0300

    Add book controller logic for book incoming requests

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Models\Book;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class BookController extends Controller
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * @param  Model $book
+     * @return void
+     */
+    public function __construct(Book $book)
+    {
+        $this->model = $book;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = $this->model->with('authors', 'publisher')->get();
+        return response()->json(['data' => $items], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBookRequest $request)
+    {
+        $item = $this->model->create($request->all());
+        $authors = $request->get('authors');
+        $item->authors()->sync($authors);
+
+        return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $item = $this->model->with('authors', 'publisher')->findOrFail($id);
+            return response()->json(['data' => $item], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => $e, 'message' => 'Item Not Found!'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBookRequest $request, $id)
+    {
+        try {
+            $item = $this->model->with('authors', 'publisher')->findOrFail($id);
+            $item->update($request->all());
+
+            $authors = $request->get('authors');
+            $item->authors()->sync($authors);
+
+            return response()->json(['data' => $item], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => $e, 'message' => 'Item Not Found!'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $item = $this->model->with('authors', 'publisher')->findOrFail($id);
+            $item->authors()->detach();
+            $item->delete();
+            return $this->index();
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => $e, 'message' => 'Item Not Found!'], 404);
+        }
+    }
+}
