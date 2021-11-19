c5b7a2581a967465308af94ca5b81d05d8bd3124
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-11-20 00:57:54 +0300

    Add validation requests for the store and update actions of books respectively

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'isbn' => 'required|digits:13|integer|unique:books,isbn',
+            'name' => 'required|min:3',
+            'year' => 'required|integer|digits:4',
+            'page' => 'required|integer',
+            'publisher_id' => 'exists:publishers,id',
+            'authors' => 'array',
+            'authors.*' => 'exists:authors,id'
+        ];
+    }
+}
